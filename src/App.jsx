33fdea2d6700c93fc97e2c9f766b2b01c17e04d0
Laassel <lassel.giovanni@gmail.com>
2024-12-04 15:04:05 +0100

    Add: Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 import Header from "./components/header";
 import styles from "./App.module.css";
 import BlogPost from "./components/BlogPost";
